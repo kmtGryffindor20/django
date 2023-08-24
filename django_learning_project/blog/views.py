1ad6c01f624e859442a2508df7266d9032e3430b
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-08-24 23:28:12 +0530

    database models

--- django_learning_project/blog/views.py
+++ django_learning_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
-posts = [
-    {
-        'author': 'KMT',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Aug 24, 2018'
-    },
-    {
-        'author': 'Kaustubh',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Aug 22, 2018'
-    }
-    
-]
+
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
