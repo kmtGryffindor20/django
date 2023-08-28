c0d5ea57da0e9fd81b5bb4229b7c4d2f222e95ab
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-08-28 15:02:11 +0530

    class based views - Detail and List View

--- django_learning_project/blog/views.py
+++ django_learning_project/blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from blog.models import Post
+from django.views.generic import ListView, DetailView
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context=context)
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
