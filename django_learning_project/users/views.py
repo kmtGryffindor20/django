ff2c5004bd591a160d747ff4252b4bff5e3f3588
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-08-28 14:39:28 +0530

    user profile update

--- django_learning_project/users/views.py
+++ django_learning_project/users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def register(request):
@@ -19,4 +20,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(request, "Profile Update!")
+            return redirect('profile')
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': user_update_form,
+        'p_form': profile_update_form
+    }
+
+    return render(request, 'users/profile.html', context=context)
