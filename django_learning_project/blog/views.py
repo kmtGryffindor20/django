7775fb073ca33c93bad94f47fe628d79199d89d6
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-08-29 15:18:39 +0530

    Post update, create and delete

--- django_learning_project/blog/views.py
+++ django_learning_project/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class PostListView(ListView):
@@ -11,7 +13,45 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    #Overriding the Form Valid Method
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
     
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    #Overriding the Form Valid Method
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
